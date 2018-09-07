1c33940f8b1caf4ad0d4f247aa0e410410fb1040
jasmeen.kaur13@gmail.com <Jasmeen.Kaur@frontiersoftware.com.au>
2018-09-07 11:03:53 +1000

    removed package folder

--- Database/Tables/CarSalesDataBaseAndTables.Create.sql
+++ Database/Tables/CarSalesDataBaseAndTables.Create.sql
@@ -87,6 +87,35 @@ GO
 ALTER TABLE [dbo].[Owners_Cars_Ref] CHECK CONSTRAINT [FK_Owner_Car_Ref]
 GO
 
+/****** Object:  Table [dbo].[Enquiry]    Script Date: 7/09/2018 10:08:38 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Enquiry](
+	[EnquiryId] [int] IDENTITY(1,1) NOT NULL,
+	[CarId] [int] NOT NULL,
+	[Name] [varchar](50) NOT NULL,
+	[PhoneNumber] [varchar](10) NOT NULL,
+	[Email] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_Enquiry] PRIMARY KEY CLUSTERED 
+(
+	[EnquiryId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Enquiry]  WITH CHECK ADD  CONSTRAINT [FK_Car_Enquiry] FOREIGN KEY([CarId])
+REFERENCES [dbo].[AdvertisedCars] ([ID])
+GO
+
+ALTER TABLE [dbo].[Enquiry] CHECK CONSTRAINT [FK_Car_Enquiry]
+GO
+
+
+
 
 
 
